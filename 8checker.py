28d23e2a98ff22ce953ecd1084677ec8e6847c2d
forms55 <80535998+forms55@users.noreply.github.com>
2024-01-05 21:03:56 +0100

    Update 8checker.py
    Due to a request of a friend of mine.

--- 8checker.py
+++ 8checker.py
@@ -1,8 +1,17 @@
-asd = []
+engedélyezettszámok = []
+kezdőérték = input("Mennyi legyen a kezdőérték?\nWhat should be the starting number? ")
+végérték = input("Mennyi legyen a végérték?\nWhat should be the ending number? ")
 
-for i in range(10000):
-    if "8" in str(i)[0]:
-        asd.append(i)
+try:
+    kezdőérték = int(kezdőérték)
+    végérték = int(végérték)
 
-print(asd)
-print(f"There are {len(asd)} numbers out there that starts with 8.")
+    for i in range(kezdőérték, végérték+1):
+        if "8" in str(i)[0]:
+            engedélyezettszámok.append(i)
+
+    print(engedélyezettszámok)
+    print(f"There are {len(engedélyezettszámok)} numbers out there that starts with 8.")
+
+except ValueError:
+    exit()
